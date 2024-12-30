55bf6db56d4671ea2b2e61ce2cf63e643658c3d0
Raul Coelho <rpc3@cin.ufpe.br>
2024-12-30 01:54:21 -0300

    fix: handling exceptions

--- script.py
+++ script.py
@@ -2,8 +2,12 @@ import yt_dlp
 from termcolor import colored
 
 def read_music_file(path):
-    with open(path, "r") as file:
-        return file.readlines()
+    try:
+        with open(path, "r") as file:
+            return file.readlines()
+    except Exception as e:
+        print(colored(f"Error reading file {path}: {e}", "red"))
+        return []
 
 def main():
     ydl_opts = {
@@ -19,10 +23,12 @@ def main():
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         songs = read_music_file("songs.txt")
         for i, song in enumerate(songs):
-            print(colored(f"\n------------------------------------  Downloading song number {i+1}  -------------------------------------\n", "green"))
-            ydl.download(song)
-            print(colored("\n---------------------------------------------  Done  -------------------------------------------------\n", "green"))
+            try:
+                print(colored(f"\n------------------------------------  Downloading song number {i+1}  -------------------------------------\n", "green"))
+                ydl.download([song.strip()])
+                print(colored("\n---------------------------------------------  Done  -------------------------------------------------\n", "green"))
+            except Exception as e:
+                print(colored(f"Error downloading song number {i+1}: {e}", "red"))
 
 if __name__ == "__main__":
     main()
-
